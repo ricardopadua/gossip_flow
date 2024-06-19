9e7e8faeea78e7800ad7bebb09ad4ede7cd1853d
Ricardo Pádua <ricardo.padua4@gmail.com>
2024-06-19 12:02:47 -0300

    chore: setup initial structure for credit_assessment service

---
+++ credit_assessment/lib/credit_assessment.ex
@@ -0,0 +1,18 @@
+defmodule CreditAssessment do
+  @moduledoc """
+  Documentation for `CreditAssessment`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CreditAssessment.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
